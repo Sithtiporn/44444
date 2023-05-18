12ea681bea00b3f594e482500335378060fb0671
Sithtiporn <122552150+Sithtiporn@users.noreply.github.com>
2023-05-18 21:44:23 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class MainClass {
+    static void Main(string[] args) {
+      
+        string[] input = Console.ReadLine().Split(' ');
+        int N = int.Parse(input[0]); 
+        int K = int.Parse(input[1]); 
+
+        int[] populations = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse); 
+
+        int maxPopulation = 0; 
+        int startIndex = -1; 
+
+        for (int i = 0; i < N - K + 1; i++) {
+            int currentPopulation = 0; 
+
+            for (int j = i; j < i + K; j++) {
+                currentPopulation += populations[j];
+            }
+
+            if (currentPopulation > maxPopulation) {
+                maxPopulation = currentPopulation;
+                startIndex = i;
+            }
+        }
+
+        Console.WriteLine(maxPopulation);
+    }
+}
